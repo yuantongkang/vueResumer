bfc161b11dc993999cfcd4e45db316741b06ebd0
Yuantong <yuantongkang@gmail.com>
2017-07-22 20:21:41 +0800

    add Topbar, ResumePreview, and ResumeEditor

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,32 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
+  <div>
+    <TopBar/>
+    <ResumeEditor/>
+    <ResumePreview/>
   </div>
 </template>
 
+
+
 <script>
+import Topbar from './components/Topbar'
+import ResumeEditor from './components/ResumeEditor'
+import ResumePreview from './components/ResumePreview'
+
+
 export default {
-  name: 'app'
+  name: 'app',
+  data() {
+    return {
+      text: "hello"
+    }
+  },
+  components: {
+    Topbar, ResumeEditor, ResumePreview
+  }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+
 </style>
