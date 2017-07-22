f4c8712df41b4c18fcb966d808eb9c953da380b5
Yuantong <yuantongkang@gmail.com>
2017-07-22 20:41:42 +0800

    使用flex布局

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <template>
-  <div>
-    <TopBar/>
-    <ResumeEditor/>
-    <ResumePreview/>
+  <div class=page>
+    <header>
+      <Topbar/>
+    </header>
+    <main>
+      <ResumeEditor/>
+      <ResumePreview/>
+    </main>
   </div>
 </template>
 
@@ -30,5 +34,29 @@ export default {
 </script>
 
 <style >
-
+ .page{
+     height: 100vh;
+     display: flex;
+     flex-direction: column;
+   }
+   .page>main{
+     flex-grow: 1;  
+   }
+ 
+   .page>main{
+     min-width: 1024px;
+     max-width: 1440px;
+     margin: 0;
+     display: flex;
+     justify-content: space-around;
+   }
+ 
+   #resumeEditor{
+     width: 35%;
+     background: #444;
+   }
+   #resumePreview{
+     width: 61.66667%;
+     background: #777;
+   }
 </style>
