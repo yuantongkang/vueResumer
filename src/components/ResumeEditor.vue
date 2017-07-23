f597a4051a3952e47ea49ecf2c71528643766918
Yuantong <yuantongkang@gmail.com>
2017-07-23 12:46:58 +0800

    tab 切换

--- src/components/ResumeEditor.vue
+++ src/components/ResumeEditor.vue
@@ -1,18 +1,75 @@
 <template>
   <div id="resumeEditor">
-    I am resumeEditor
+    <nav>
+      <ol>
+        <li v-for="(item,index) in resume.visibleItems" :class="{active: item === selected}" @click="selected = item" :key="index">
+          {{index}}
+        </li>
+      </ol>
+    </nav>
+    <ol class="panels">
+      <li v-for="item in resume.visibleItems" v-show="item === selected" :key="item">
+        {{resume[item]}}
+      </li>
+    </ol>
   </div>
 </template>
  
- <script>
+<script>
 export default {
-  name: 'ResumeEditor'
+  name: 'ResumeEditor',
+  data() {
+    return {
+      selected: 'bio',
+      resume: {
+        visibleItems: ['bio', 'work history', 'education', 'projects', 'awards', 'contacts', 'others'],
+        bio: {
+          name: '',
+          city: '',
+          title: ''
+        },
+       'work history': [],
+        education: [],
+        projects: [],
+        awards: [],
+        contacts: [],
+        others: []
+      }
+    }
+  }
 }
+
 </script>
  
- <style>
+ <style lang="scss">
 #resumeEditor {
-  background:#ffffff;
-  box-shadow:0 1px 3px 0 rgba(0,0,0,0.25)
+  background: #ffffff;
+  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.25);
+  display: flex;
+  flex-direction: row;
+  >nav {
+    width: 80px;
+    background: black;
+    overflow: auto;
+    color: white;
+    >ol {
+      >li {
+        height: 48px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin-top: 16px;
+        margin-bottom: 16px;
+        &.active {
+          background: white;
+          color: black;
+        }
+      }
+    }
+  }
+}
+
+ol {
+  list-style: none;
 }
 </style>
